007859ef9cd5700131c1928e9e1e7e7001806254
Tiago Silva <tiagosilva_live_2016@outlook.com>
2025-03-11 16:02:54 -0300

    criado button no no home, add style no app.css e adicionado classse a main li

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,26 +5,28 @@ export const Home = () => {
     const [pokemons, setPokemons] = useState({});
 
     useEffect(() => {
-        const fecthData = async () => {
+        const fecthHome = async () => {
             const pokemons = await dataPokemons()
-            console.log(pokemons);
+            // console.log(pokemons);
             setPokemons(pokemons)
         }
-        fecthData()
+        fecthHome()
     }, [])
 
     return (
         <main>
-            <h1>Lista de Pokemon</h1>
+            <h1>Lista de Pokemons</h1>
             <ul>
                 {pokemons.length > 0 ? (pokemons.map((pokemon) => (
-                    <li key={pokemon.id}>
+                    <li key={pokemon.id} className="cards">
                         <h3>{pokemon.name}</h3>
                         <img src={pokemon.sprites.other["official-artwork"]?.front_default ?? pokemon.sprites.other["official-artwork"]?.front_shiny} alt={pokemon.name} />
                     </li>))
                 ) : (<p>Nenhum Pokemon encontrado.</p>)
                 }
             </ul>
+
+                <button>Clique aki para carregar mais Pokemons</button>
         </main>
     );
 }
